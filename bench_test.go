81b8cab948298dcd457bb72c1658129068bb4ad1
Kirill Safronov <hoskeowl@gmail.com>
2022-10-14 14:17:59 +0200

    Add levels to benchmark test

--- bench_test.go
+++ bench_test.go
@@ -2,18 +2,57 @@ package goStringSlicer
 
 import "testing"
 
-var example string = "😸 Some very long string with nice unicode😼"
+var exampleSmall string = "😸😼"
+var exampleMedium string = "😸 Some very long string with nice unicode😼"
+var exampleLarge string = "😸 Чёрная дыра́ — область пространства-времени, гравитационное притяжение которой настолько велико, что покинуть её не могут даже объекты, движущиеся со скоростью света, в том числе кванты самого света. Граница этой области называется горизонтом событий. В простейшем случае сферически симметричной чёрной дыры он представляет собой сферу с радиусом Шварцшильда, который считается характерным размером чёрной дыры."
 
-func BenchmarkSliceThroughRunes(b *testing.B) {
+const (
+	smallBegin  int = 0
+	smallEnd    int = 1
+	mediumBegin int = 4
+	mediumEnd   int = 18
+	largeBegin  int = 10
+	largeEnd    int = 120
+)
+
+func BenchmarkSmallSliceThroughRunes(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		runes := []rune(example)
-		part := runes[4:18]
+		runes := []rune(exampleSmall)
+		part := runes[smallBegin:smallEnd]
 		_ = string(part)
 	}
 }
 
-func BenchmarkSliceWithFunction(b *testing.B) {
+func BenchmarkSmallSliceWithFunction(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_, _ = SliceString(example, 4, 18)
+		_, _ = SliceString(exampleSmall, smallBegin, smallEnd)
+	}
+}
+
+func BenchmarkMediumSliceThroughRunes(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		runes := []rune(exampleMedium)
+		part := runes[mediumBegin:mediumEnd]
+		_ = string(part)
+	}
+}
+
+func BenchmarkMediumSliceWithFunction(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, _ = SliceString(exampleMedium, mediumBegin, mediumEnd)
+	}
+}
+
+func BenchmarkLargeSliceThroughRunes(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		runes := []rune(exampleLarge)
+		part := runes[largeBegin:largeEnd]
+		_ = string(part)
+	}
+}
+
+func BenchmarkLargeSliceWithFunction(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, _ = SliceString(exampleLarge, largeBegin, largeEnd)
 	}
 }
